01ea4187acfee5d477bc7269f8fb55fe9918aa69
sreeramsdev <162151401+sreeramsdev@users.noreply.github.com>
2024-05-02 12:50:57 +0530

    Update question2.js

--- Test 2-05-2024/question2.js
+++ Test 2-05-2024/question2.js
@@ -4,13 +4,8 @@
 
 // Ans: [ 1, 2, 3, 4, 5 ]
 
-var arr=[1,2,3,3,4,5,5]
-var arr1=[]
-for(let i=0;i<arr.length;i++){
-    if(arr[i]==arr[i+1]){
-     arr1.push(arr[i])
-        
-    }
-   
-}
-console.log(arr1)
+const array = [1, 2, 3, 3, 4, 5, 5];
+
+const uniqueArray = [...new Set(array)];
+
+console.log(uniqueArray);
